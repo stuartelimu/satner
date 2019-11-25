dae97d0e29c3b5d7a54a54231e84b8e0f11fba5c
ElimuStuart <stuartelimu@gmail.com>
2019-11-25 21:43:29 +0300

    add authentication backend

--- satner/settings.py
+++ satner/settings.py
@@ -131,3 +131,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.authentication.EmailAuthBackend',
+]
