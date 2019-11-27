68b797cb4d98edcd18834b709ea19ed4d841ec95
ElimuStuart <stuartelimu@gmail.com>
2019-11-27 20:23:47 +0300

    setup s3 bucket

--- satner/settings.py
+++ satner/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'marketing',
 
     'hitcount',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -153,3 +154,17 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_BACKEND = config('EMAIL_BACKEND')
+
+# aws
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
