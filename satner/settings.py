bb6c67db366f06309b066bbcd8e8b572eaa372f5
ElimuStuart <stuartelimu@gmail.com>
2019-11-27 20:58:50 +0300

    add Procfile, ready for deployment

--- satner/settings.py
+++ satner/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config, Csv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -185,3 +186,4 @@ else:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+django_heroku.settings(locals())
